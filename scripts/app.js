a0dc537590040f90c8d11bc8a5f3e5afa362607b
kbazant <krizz100@yahoo.com>
2016-10-09 17:24:13 +0100

    added mainCtrl controller

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,6 @@
 angular.module("todoListApp", []);
+.controller('mainCtrl', function($scope){
+	$scope.helloworld = function() {
+		console.log("Hello there! This is the helloWorld controller function, in the mail Ctrl!");
+	};
+});
