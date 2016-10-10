f2d92e7d37de5711801626c09e07cdbdd2d1035b
kbazant <krizz100@yahoo.com>
2016-10-11 00:54:27 +0100

    added controller and scope

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,23 @@
-angular.module("todoListApp", []);
+angular.module("todoListApp", [])
 .controller('mainCtrl', function($scope){
-	$scope.helloworld = function() {
+	$scope.helloWorld = function() {
 		console.log("Hello there! This is the helloWorld controller function, in the mail Ctrl!");
 	};
+})
+	
+.controller('coolCtrl', function($scope){
+	$scope.whoAmI = function() {
+		console.log("Hello there! This is the coolCtrl function!");
+	};	
+	
+$scope.helloWorld = function(){
+	console.log("This is not the main ctrl!");
+}
+	
+})
+.controller('imASibling', function($scope){
+	
+	$scope.foobar = 1234;
+	
+	//do other cool stuff!
 });
