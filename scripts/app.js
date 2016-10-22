c5bc127cfe9fe4b57f7abb28ab0a13a9e6ef595c
kbazant <krizz100@yahoo.com>
2016-10-22 19:51:29 +0200

    added ng-repeat to inject HTML for every data element

--- scripts/app.js
+++ scripts/app.js
@@ -1,23 +1,17 @@
 angular.module("todoListApp", [])
-.controller('mainCtrl', function($scope){
+
+.controller('mainCtrl', function($scope) {
 	$scope.helloWorld = function() {
 		console.log("Hello there! This is the helloWorld controller function, in the mail Ctrl!");
 	};
-})
+
+	$scope.todos = [
+		{"name": "clean the house"},
+		{"name": "water the dog"},
+		{"name": "feed the lawn"},
+		{"name": "pay dem bills"},
+		{"name": "run"},
+		{"name": "swim"}
+	]
 	
-.controller('coolCtrl', function($scope){
-	$scope.whoAmI = function() {
-		console.log("Hello there! This is the coolCtrl function!");
-	};	
-	
-$scope.helloWorld = function(){
-	console.log("This is not the main ctrl!");
-}
-	
-})
-.controller('imASibling', function($scope){
-	
-	$scope.foobar = 1234;
-	
-	//do other cool stuff!
 });
