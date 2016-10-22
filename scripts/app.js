681197238b9472afbee80fd08f332f78c2d7bab3
kbazant <krizz100@yahoo.com>
2016-10-22 21:44:12 +0200

    changed scope and console.log

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,8 @@
 angular.module("todoListApp", [])
 
 .controller('mainCtrl', function($scope) {
-	$scope.helloWorld = function() {
-		console.log("Hello there! This is the helloWorld controller function, in the mail Ctrl!");
+	$scope.learningNgChange = function() {
+		console.log("An input changed!");
 	};
 
 	$scope.todos = [
