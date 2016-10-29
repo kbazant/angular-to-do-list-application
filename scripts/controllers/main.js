2be184133b05f793306600eadc842bda3cb2bbea
kbazant <krizz100@yahoo.com>
2016-10-29 19:41:04 +0100

    removed closing semicolon

--- scripts/controllers/main.js
+++ scripts/controllers/main.js
@@ -1,28 +1,32 @@
 'use strict';
 
 angular.module('todoListApp')
-.controller('mainCtrl', function($scope, dataService) {
-	$scope.addTodo = function() {
-		var todo = {name: "This is a new todo."};
-		$scope.todos.unshift(todo);
-	};
-	
-	$scope.learningNgChange = function() {
-		console.log("An input changed!");
-	};
-	
-	dataService.getTodos(function(response) {
+	.controller('mainCtrl', function ($scope, dataService) {
+		$scope.addTodo = function () {
+			var todo = {
+				name: "This is a new todo."
+			};
+			$scope.todos.unshift(todo);
+		};
+
+		$scope.helloWorld = dataService.helloWorld;
+
+		dataService.getTodos(function (response) {
 			console.log(response.data);
 			$scope.todos = response.data;
-	});
-	
-	$scope.deleteTodo = function(todo, $index) {
-	 dataService.deleteTodo(todo);
-		$scope.todos.splice($index, 1);	
-	};
-	
-	$scope.saveTodo = function(todo) {
-		dataService.saveTodo(todo);
-	};
-	
-});
+		});
+
+		$scope.deleteTodo = function (todo, $index) {
+			dataService.deleteTodo(todo);
+			$scope.todos.splice($index, 1);
+		};
+
+		$scope.saveTodos = function () {
+			var filteredTodos = $scope.todos.filter(function (todo) {
+				if (todo.edited) {
+					return todo;
+				};
+			});
+			dataService.saveTodos(filteredTodos);
+		};
+	})
